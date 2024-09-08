95adddfc70255d9a924c9445d964b41c81dfb404
Moyartu Manley <mmanley@macalester.edu>
2024-09-08 17:25:22 -0500

    Added additional comments

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,2 @@
 print("Hello World!")
-# Thank you for the beautiful comment Wren. Very cool.
+# Thank you for the beautiful comment Wren. Very cool. Thank you for the corrections as well. I really appreciate it.
