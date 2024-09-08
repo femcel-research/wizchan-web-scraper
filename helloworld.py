a33f8ebcb5139d63e19d49c47756ca4551f3a22b
wkuratana <wkuratan@macalester.edu>
2024-09-08 17:27:12 -0500

    Education

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,3 @@
 print("Hello World!")
 # Thank you for the beautiful comment Wren. Very cool. Thank you for the corrections as well. I really appreciate it.
+print("'i' before 'e' except after 'c'!")
