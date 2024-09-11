171ae2d570783b73e96eb8ea45f7482ac86e08bd
Moyartu Manley <mmanley@macalester.edu>
2024-09-10 19:33:07 -0500

    Experimenting with BeautifulSoup methods

--- main.py
+++ main.py
@@ -5,6 +5,19 @@ URL = "https://crystal.cafe/b/res/293815.html#294827"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
+threadContents = soup.find(class_= "body")
+
+#still working on printing all thread replies
+threadReplies = soup.find_all('div', class_= "post reply")
+
+print(threadContents.prettify())
+#print(threadReplies.prettify())
+
+#for replies in threadReplies:
+    #replyContents = threadReplies.find_all('div', class_="body")
+
+    #for comment in replyContents:
+        #print(comment.text())
 
 #print(page.text)
-print(soup.prettify())
+#print(soup.prettify())
