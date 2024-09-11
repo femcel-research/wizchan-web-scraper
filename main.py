5ead499cdf08aa8ea14af06b0a0f3618481a3ca8
Moyartu Manley <mmanley@macalester.edu>
2024-09-10 19:09:18 -0500

    Added BeautifulSoup Library and tested out .prettify method

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests
+from bs4 import BeautifulSoup
 
 URL = "https://crystal.cafe/b/res/293815.html#294827"
 page = requests.get(URL)
 
-print(page.text)
+soup = BeautifulSoup(page.content, "html.parser")
+
+#print(page.text)
+print(soup.prettify())
