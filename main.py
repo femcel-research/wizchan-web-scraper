48fc23142d293c50643ea70671d07219513953b9
Moyartu Manley <mmanley@macalester.edu>
2024-09-10 19:50:38 -0500

    Added comments regarding BS4

--- main.py
+++ main.py
@@ -4,25 +4,20 @@ from bs4 import BeautifulSoup
 URL = "https://crystal.cafe/b/res/293815.html#294827"
 page = requests.get(URL)
 
+#Creates a soup object.
 soup = BeautifulSoup(page.content, "html.parser")
+
+#Finds the first instance of a page element with the class "body". Within a specific thread page, this most likely be the original thread.
 threadContents = soup.find(class_= "body")
 
+#Prints the text from the original thread content, excluding the HTML tags.
 print("Original Thread Content: " + threadContents.get_text() + '\n')
 
-#still working on printing all thread replies
+#Finds every page element with the class "post reply" and returns it in an array.
 print("Replies:")
-threadReplies = soup.find_all(class_= "post reply")
+threadReplies = soup.find_all(class_= "post reply") 
+
+#For each reply in the array, the text is taken from the array element and printed. Thus excluding the HTML tags.
 for reply in threadReplies:
     print(reply.get_text())
 
-
-
-
-#for replies in threadReplies:
-    #replyContents = threadReplies.find_all('div', class_="body")
-
-    #for comment in replyContents:
-        #print(comment.text())
-
-#print(page.text)
-#print(soup.prettify())
