9de688f7afe762eb3c3fa2997d9ca06ff79e6619
Moyartu Manley <mmanley@macalester.edu>
2024-09-12 03:31:53 -0500

    Demo scraper now creates and writes to a text file.

--- main.py
+++ main.py
@@ -8,16 +8,26 @@ page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 
 #Finds the first instance of a page element with the class "body". Within a specific thread page, this most likely be the original thread.
-threadContents = soup.find(class_= "body")
+originalPost = soup.find(class_= "post op")
+
+with open('testfile.txt', 'w', encoding='utf-8') as outputFile:
+    outputFile.write("Original Post: " + '\n')
+    outputFile.write(originalPost.get_text() + '\n\n')
+    outputFile.write("Replies:" + '\n')
+
 
 #Prints the text from the original thread content, excluding the HTML tags.
-print("Original Thread Content: " + threadContents.get_text() + '\n')
+#print("Original Post: " + '\n' + originalPost.get_text() + '\n')
 
 #Finds every page element with the class "post reply" and returns it in an array.
-print("Replies:")
-threadReplies = soup.find_all(class_= "post reply") 
+postReplies = soup.find_all(class_= "post reply") 
 
 #For each reply in the array, the text is taken from the array element and printed. Thus excluding the HTML tags.
-for reply in threadReplies:
-    print(reply.get_text())
+for reply in postReplies:
+    #print(reply.get_text())
+    with open('testfile.txt', 'a', encoding='utf-8') as outputFile:
+        outputFile.write(reply.get_text() + '\n')
+
+with open('testfile.txt', 'r', encoding='utf-8') as outputFile:
+        print(outputFile.read())
 
