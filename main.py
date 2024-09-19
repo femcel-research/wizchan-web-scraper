95b08084048650b6c8f8fb03ce284686d2a1189c
Moyartu Manley <mmanley@macalester.edu>
2024-09-19 18:40:25 -0500

    working on gathering html elems

--- main.py
+++ main.py
@@ -17,38 +17,50 @@ threadNumber = soup.find(class_= "intro").get('id')
 #install pywebcopy and lxml[html_clean] package for use.
 
 #Saves a copy of the URL to the HTML folder. Contents are nested in a folder named after the specific thread number.
-save_webpage(
-    url = URL,
-   project_folder = "./HTML",
-   project_name = "thread_" + threadNumber,
-   bypass_robots=True,
-   debug=True,
-   open_in_browser=False,
-   delay=None,
-   threaded = True #if set to false downloads will be incredibly slow.
-)
+# save_webpage(
+#     url = URL,
+#    project_folder = "./HTML",
+#    project_name = "thread_" + threadNumber,
+#    bypass_robots=True,
+#    debug=True,
+#    open_in_browser=False,
+#    delay=None,
+#    threaded = True #if set to false downloads will be incredibly slow.
+# )
 
 
 # Finds the first instance of a page element with the class "body". 
 # Within a specific thread page, this most likely be the original thread.
 originalPost = soup.find(class_= "post op")
+threadIntro = soup.find(class_="intro")
+originalPostID= threadIntro['id']
+#userIDs = 
+#imageIDs=
+#dateCollected = 
+
+#TODO finish datePosted
+# datePosted= soup.find(class_="post_no date link").title.get_text()
+#print(datePosted)
+board = soup.header.h1.get_text()
+
+
 
 # Opens a writeable text file, writes related headers and original post content on it and then closes file.
-with open('testfile.txt', 'w', encoding='utf-8') as outputFile:
-    outputFile.write("Original Post: " + '\n')
-    outputFile.write(originalPost.get_text() + '\n\n')
-    outputFile.write("Replies:" + '\n')
+# with open('testfile.txt', 'w', encoding='utf-8') as outputFile:
+#     outputFile.write("Original Post: " + '\n')
+#     outputFile.write(originalPost.get_text() + '\n\n')
+#     outputFile.write("Replies:" + '\n')
 
 # Finds every page element with the class "post reply" and returns it in an array.
 postReplies = soup.find_all(class_= "post reply") 
 
 # For each reply in the array, the text is taken from the array element and appended into an open text file. 
 # HTML tags are excluded, and the file is closed once the code is executed.
-for reply in postReplies:
-    with open('testfile.txt', 'a', encoding='utf-8') as outputFile:
-        outputFile.write(reply.get_text() + '\n')
+# for reply in postReplies:
+#     with open('testfile.txt', 'a', encoding='utf-8') as outputFile:
+#         outputFile.write(reply.get_text() + '\n')
 
 # Opens, prints contents of text file in terminal for debugging, and then closes the file once code is executed.
-with open('testfile.txt', 'r', encoding='utf-8') as outputFile:
-        print(outputFile.read())
+# with open('testfile.txt', 'r', encoding='utf-8') as outputFile:
+#         print(outputFile.read())
 
