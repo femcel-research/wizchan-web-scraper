1a2f2c3c6fd80c32bba5e26d8f8db0af59ceb272
wkuratana <wkuratan@macalester.edu>
2024-09-19 19:17:25 -0500

    Added comments and revised UrlPullerTest to update the puller object with a new file

--- UrlPuller.py
+++ UrlPuller.py
@@ -2,6 +2,7 @@ class UrlPuller:
     """Extracts URLs from a text file"""
     
     def __init__(self, filename):
+        """URLs are taken from filename and turned into a list, with values separated by line"""
         self.filename = filename
         self.url_list = []
         self.url_index = 0
@@ -10,11 +11,21 @@ class UrlPuller:
                 self.url_list.append(line)
 
     def next_url(self):
+        """Shifts index for when you want to call get_url on the next entry"""
         self.url_index = self.url_index + 1
 
     def get_url(self):
+        """Returns URL at current index"""
         return self.url_list[self.url_index]
     
     def get_size(self):
+        """Returns the number of entries in the list created from the file specified on initialization"""
         return len(self.url_list)
     
+    def set_new_file_list(self, filename):
+        self.filename = filename
+        self.url_list = []
+        self.url_index = 0
+        with open(self.filename, 'r') as file:
+            for line in file:
+                self.url_list.append(line)
--- UrlPullerTest.py
+++ UrlPullerTest.py
@@ -1,10 +1,16 @@
 import UrlPuller
 import os
 
-puller_file = os.path.join(os.getcwd(), 'URLs', 'list.txt')
+puller_file = os.path.join('URLs', 'list.txt')
 puller = UrlPuller.UrlPuller(puller_file)
-
+print("first loop")
 for i in range(puller.get_size()):
     print(puller.get_url())
     puller.next_url()
 
+puller_file_2 = os.path.join('URLs', 'list2.txt')
+puller.set_new_file_list(puller_file_2)
+print("second loop")
+for i in range(puller.get_size()):
+    print(puller.get_url())
+    puller.next_url()
