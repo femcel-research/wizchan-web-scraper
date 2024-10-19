96cd4eb924e9bea1439e0e5101b42b3121904243
moyartumanley <mmanley@macalester.edu>
2024-10-18 22:59:55 -0500

    Very minor formatting

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 from utils import TextSaver
-#from utils import Webcopy
+
+# from utils import Webcopy
 from utils import UrlPuller
 from utils import WebpageSaver
 from utils import MetaCollector
 
 # Website URL
-#URL = "https://crystal.cafe/b/res/293815.html#294827"
-#URL = "https://crystal.cafe/b/res/294499.html#q294499"
-#URL = "https://crystal.cafe/b/res/140376.html#q140376"
+# URL = "https://crystal.cafe/b/res/293815.html#294827"
+# URL = "https://crystal.cafe/b/res/294499.html#q294499"
+# URL = "https://crystal.cafe/b/res/140376.html#q140376"
 # URL = "https://crystal.cafe/feels/res/63992.html#116946"
-#URL = "https://crystal.cafe/b/res/273609.html#295472"
+# URL = "https://crystal.cafe/b/res/273609.html#295472"
 URL = "https://crystal.cafe/b/res/295503.html#q295503"
 
-page = requests.get(URL, stream = True)
+page = requests.get(URL, stream=True)
 
 # Creates a soup object.
 soup = BeautifulSoup(page.content, "html.parser")
@@ -22,32 +23,32 @@ soup = BeautifulSoup(page.content, "html.parser")
 # Returns top portion of original post
 threadIntro = soup.find(class_="intro")
 
-#Should hopefully always return the thread number, given that for crystal.cafe, the thread number is always the first id in the intro class
-threadNumber = soup.find(class_= "intro").get('id')
+# Should hopefully always return the thread number, given that for crystal.cafe, the thread number is always the first id in the intro class
+threadNumber = soup.find(class_="intro").get("id")
 
-# Finds the first instance of a page element with the class "body". 
+# Finds the first instance of a page element with the class "body".
 # Within a specific thread page, this most likely be the original thread.
-originalPost = soup.find(class_= "post op")
+originalPost = soup.find(class_="post op")
 
 # Variable holds the ID of the original post.
-originalPostID= threadIntro['id']
+originalPostID = threadIntro["id"]
 
 # Finds every page element with the class "post reply" and returns it in an array.
-postReplies = soup.find_all(class_= "post reply")
+postReplies = soup.find_all(class_="post reply")
 
 # Returns an array containing the ID for each reply.
-postReplyIds = [reply['id'] for reply in postReplies]
+postReplyIds = [reply["id"] for reply in postReplies]
 
 # Returns an array containing all of the image data for post images
-allImages = soup.find_all('img', class_="post-image")
+allImages = soup.find_all("img", class_="post-image")
 
 # Returns the sources for each post image
-imageSources = [image['src'] for image in allImages]
+imageSources = [image["src"] for image in allImages]
 
 # Returns imageboard name
 board = soup.header.h1.get_text()
 
-#TODO dateCollected is captured in the HTML file when local files are downloaded, time is in UTC
+# TODO dateCollected is captured in the HTML file when local files are downloaded, time is in UTC
 
 # Saves text from page in a .txt file named after the string arg
 # textSaver = TextSaver("testfile.txt", originalPost, postReplies, threadNumber)
--- utils/MetaCollector.py
+++ utils/MetaCollector.py
@@ -5,72 +5,76 @@ import requests
 import datetime
 import os
 
+
 class MetaCollector:
     """Collects metadata from a website and stores it in a JSON file"""
 
     def __init__(self, URL, pageName):
-        #Website info
+        # Website info
         self.URL = URL
         self.pageName = pageName
         self.response = requests.get(self.URL)
 
-        #File path
-        self.folder_path = './web-scraper/data/meta'
+        # File path
+        self.folder_path = "./web-scraper/data/meta"
         self.file_name = "thread_" + self.pageName + ".json"
-        self.file_path = os.path.join(self.folder_path, self.file_name) 
+        self.file_path = os.path.join(self.folder_path, self.file_name)
 
     def date_to_JSON(self):
         """Captures date published, date updated, and date scraped from a specified website"""
-        
-        #Uses htmldate lib to find original and update dates
-        publish_Date = find_date(self.response.content, extensive_search=True, original_date= True)
-        update_Date = find_date(self.response.content, extensive_search= False, original_date= False)
-        
-        #Assumption is that each time this func is run during scrape, it will capture the time of scrape
+
+        # Uses htmldate lib to find original and update dates
+        publish_Date = find_date(
+            self.response.content, extensive_search=True, original_date=True
+        )
+        update_Date = find_date(
+            self.response.content, extensive_search=False, original_date=False
+        )
+
+        # Assumption is that each time this func is run during scrape, it will capture the time of scrape
         scrape_date = datetime.datetime.now()
-        formatted_date = scrape_date.strftime('%Y-%m-%d')
+        formatted_date = scrape_date.strftime("%Y-%m-%d")
 
         dates = {
             "date published": publish_Date,
             "date updated": update_Date,
-            "date scraped": formatted_date
-            }
-        
+            "date scraped": formatted_date,
+        }
+
         return dates
-        
-    
+
     def page_info_to_JSON(self):
         """Captures page URL, title, description, keywords, site info"""
 
-        page = requests.get(self.URL, stream = True)
+        page = requests.get(self.URL, stream=True)
         soup = BeautifulSoup(page.content, "html.parser")
 
-        #If meta keywords has content create a variable with that content, otherwise set to empty string
-        meta_keywords = soup.find('meta', attrs={'name': 'keywords'})
+        # If meta keywords has content create a variable with that content, otherwise set to empty string
+        meta_keywords = soup.find("meta", attrs={"name": "keywords"})
         if meta_keywords:
-            keywords = meta_keywords['content']
+            keywords = meta_keywords["content"]
         else:
             keywords = ""
 
-        #If meta description has content create a variable with that content, otherwise set to empty string
-        meta_description = soup.find('meta', attrs={'name': 'description'})
+        # If meta description has content create a variable with that content, otherwise set to empty string
+        meta_description = soup.find("meta", attrs={"name": "description"})
         if meta_description:
-            description = meta_description['content']
+            description = meta_description["content"]
         else:
             description = ""
 
         info = {
-            "URL": page.url, 
-            "board": soup.title.string, #for cc its safe to assume each title will be the board name
-            "thread number": soup.find(class_= "intro").get('id'),
+            "URL": page.url,
+            "board": soup.title.string,  # for cc its safe to assume each title will be the board name
+            "thread number": soup.find(class_="intro").get("id"),
             "description": description,
-            "keywords": keywords 
+            "keywords": keywords,
         }
         return info
-    
+
     def meta_dump(self):
         """Dumps website metadata into a JSON file"""
-        metadata = {**self.page_info_to_JSON(),**self.date_to_JSON()}
-        
-        with open(self.file_path, "w",  encoding='utf-8') as f:
+        metadata = {**self.page_info_to_JSON(), **self.date_to_JSON()}
+
+        with open(self.file_path, "w", encoding="utf-8") as f:
             json.dump(metadata, f, indent=2, ensure_ascii=False)
