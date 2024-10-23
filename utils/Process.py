0c428987a3e742b5d0102fee9ea553d9baaae7df
wkuratana <wkuratan@macalester.edu>
2024-10-22 19:40:15 -0500

    Process.py outline

---
+++ utils/Process.py
@@ -0,0 +1,10 @@
+# Input txt file with URLs
+
+
+# For each URL
+    # HTML file
+    # JSON metadata file
+    # JSON thread content file
+    # Add URL to list of processed URLs
+
+# Save list of processed URLs to txt file
