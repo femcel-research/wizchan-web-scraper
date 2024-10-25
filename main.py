3d281b7b6c7d305b1884c8e61d6d4f40cc84f4d9
wkuratana <wkuratan@macalester.edu>
2024-10-24 21:10:31 -0500

    Added note and processed more URLs

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import os
 from utils import Process
 
-list_path = os.path.abspath("./data/lists/urls.txt")
+# Please note when making lists of URLs:
+# - One URL per line
+# - Check the thread number against the thread numbers of URLs in processed 
+#   (not the message number! For example, in https://crystal.cafe/feels/res/117906.html#117998,
+#   the thread number is 117906, not 117998!)
+
+list_path = os.path.abspath("./data/lists/urls2.txt")
 process = Process(list_path)
 process.process_current_list()
 print("processed")
