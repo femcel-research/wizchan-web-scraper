798808715d15b0d6c7c3e48b6bafc17fbd5c6009
wkuratana <wkuratan@macalester.edu>
2024-10-24 19:24:21 -0500

    Process.py version 1

--- utils/Process.py
+++ utils/Process.py
@@ -10,41 +10,45 @@ class Process:
     """Takes in a text file of URLs then loops through them, 'processing' each one"""
 
     def __init__(self, url_list_file):
+        """Pass in the list.txt of URLs you want to be processed"""
         self.puller = UrlPuller.UrlPuller(url_list_file)
 
-    # Save list of processed URLs to txt file
     def log_processed_url(url):
+        """Save list of processed URLs to txt file in data/processed"""
         with open("./web-scraper/data/processed/processed.txt", "a") as file:
             file.write(url + '\n')
 
-    def get_soup_object(page):
-        soup = BeautifulSoup(page.page.content, "html.parser")
+    def make_soup_object(page):
+        soup = BeautifulSoup(page.content, "html.parser")
         return soup
 
     def make_thread_directory(id):
+        """Creates a new folder to associate with the thread being processed"""
         thread_folder_path = "./web-scraper/data/" + id
         os.mkdir(thread_folder_path)
 
     def process_current_list(self):
-        # For each URL
+        """For each URL in the list, get HTML, metadata JSON, and content JSON"""
+
         for i in range(self.puller.get_size()):
             # Gets page from URL and makes a new directory for the thread
             url = self.puller.get_url
             page = requests.get(url, stream=True)  
-            id = self.get_soup_object(page).find(class_="intro").get("id")
+            soup = self.make_soup_object(page)
+            id = soup.find(class_="intro").get("id")
             self.make_thread_directory(id)
 
             # HTML file
-            HTMLCollection = HTMLCollector(page)
-            HTMLCollection.saveHTML()
+            html = HTMLCollector(soup)
+            html.saveHTML()
 
             # JSON metadata file
-            metaCollector = MetaCollector(page)
-            metaCollector.meta_dump()
+            meta = MetaCollector(page, soup)
+            meta.meta_dump()
 
             # JSON thread content file
-            textSaver = TextCollector("testfile.txt", page)
-            textSaver.write_thread()
+            content = TextCollector(soup)
+            content.write_thread()
 
             # Add URL to list of processed URLs
             self.log_processed_url(url)
