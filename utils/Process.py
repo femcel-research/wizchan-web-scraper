fb5404d3f8b62d63d3461e651b0f8c01a9fac81a
wkuratana <wkuratan@macalester.edu>
2024-10-31 19:14:04 -0500

    Working on handling duplicate scans; check_thread_id, etc.

--- utils/Process.py
+++ utils/Process.py
@@ -5,6 +5,9 @@ from utils import TextCollector
 from utils import HomePageScraper
 import os
 import requests
+import datetime
+from datetime import date
+from htmldate import find_date
 
 class Process:
     """Takes in a homepage URL then loops through the links on it, 'processing' each one"""
@@ -24,8 +27,36 @@ class Process:
 
     def make_thread_directory(self, id):
         """Creates a new folder to associate with the thread being processed"""
-        self.thread_folder_path = "./data/" + id
-        os.makedirs(self.thread_folder_path, exist_ok=True)
+        thread_folder_path = "./data/" + id
+        os.makedirs(thread_folder_path, exist_ok=True)
+
+    def make_scan_directory(self, id):
+        scan_path = "/" + datetime.today().strftime('%Y-%m-%dT%H:%M:%S')
+        os.makedirs("./data/" + id +"/" + scan_path,exist_ok = True)
+
+    def check_thread_id(self, id):
+        if(os.path.exists("./data/" + id )):
+            return True
+        else: 
+            return False
+        
+    def check_scan(self, page):
+        """Return True if the most recent scan is not up-to-date, False if it's up-to-date"""
+        update_date = find_date(
+            # Assigns update_date to the update date of page (the page being checked)
+            page.content,
+            extensive_search=False,
+            original_date=False,
+            outputformat="%Y-%m-%d %H:%M:%S",
+        )
+        
+        # previous_update_date = https://stackoverflow.com/questions/54491156/validate-json-data-using-python
+            # Look at the thread directory, look at most recent scan folder, check json metadata update date
+
+        if update_date is previous_update_date:
+            return False
+        else:
+            return True
 
     def process_current_list(self):
         """For each URL in the list, get thread HTML, metadata JSON, and content JSON"""
@@ -38,24 +69,32 @@ class Process:
             intro_element = soup.find(class_="intro")
             
             if intro_element is not None:
+                id = intro_element.get("id")
+                if self.check_thread_id(id): # return true if no thread ID folder
+                    self.make_thread_directory(id)
+                if self.check_scan(page): # return true if not-up-to-date
+                    self.make_scan_directory(id)
+                    
+                    # HTML file
+                    thread = HTMLCollector(soup, self.thread_folder_path)
+                    (thread.saveHTML())
+
+                    # JSON metadata file
+                    meta = MetaCollector(page, soup, self.thread_folder_path)
+                    (meta.meta_dump())
+
+                    # JSON thread content file
+                    content = TextCollector(soup, self.thread_folder_path)
+                    (content.write_thread())
+
+                    # Add URL to list of processed URLs
+                    self.log_processed_url(url)
+
+            
+
+
                 threadNumber = intro_element.get("id")
                 self.make_thread_directory(threadNumber)
-                
-            else:
-                self.puller.next_url()
-                continue
 
-            # HTML file
-            thread = HTMLCollector(soup, self.thread_folder_path)
-            (thread.saveHTML())
 
-            # JSON metadata file
-            meta = MetaCollector(page, soup, self.thread_folder_path)
-            (meta.meta_dump())
-
-            # JSON thread content file
-            content = TextCollector(soup, self.thread_folder_path)
-            (content.write_thread())
-
-            # Add URL to list of processed URLs
-            self.log_processed_url(url)
+            
