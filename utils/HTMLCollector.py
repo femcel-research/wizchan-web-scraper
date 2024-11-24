3f71e0c9f68615a3e32631b33072a3efac4c9fb3
moyartumanley <mmanley@macalester.edu>
2024-11-23 23:06:03 -0600

    Functional + Added id as param. Changed string append to .format()

--- utils/HTMLCollector.py
+++ utils/HTMLCollector.py
@@ -6,18 +6,14 @@ import os
 #TODO no longer need to pass page
 class HTMLCollector:
     """Saves a URL's HTML into a file"""
-    def __init__(self, soup, folder_path):
+    def __init__(self, soup, id, folder_path):
         self.soup = soup
+        self.id = id
         self.folder_path = folder_path
 
     def saveHTML(self):
         """Saves HTML"""
-        #soup = BeautifulSoup(self.page.content, "html.parser")
-        # Should hopefully always return the thread number, given that for crystal.cafe, the thread number is always the first id in the intro class
-        threadNumber = self.soup.find(class_="intro").get("id")
-        
-        # self.folder_path = './web-scraper/data/HTML'
-        file_name = "thread_" + threadNumber + ".html"
+        file_name = "thread_{}.html".format(self.id)
         file_path = os.path.join(self.folder_path, file_name) 
         
         with open(file_path, "w") as f:
